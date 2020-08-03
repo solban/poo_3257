67da2cebcaa84e19fc91729727e9576644cd4efa
solban <javierespinosa316@aragon.unam.mx>
2020-08-03 17:14:44 -0500

    clase del dia 3 de agosto del 2020

---
+++ Sesion_3_agosto/src/ico/fes/poo/Teclado.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ico.fes.poo;
+
+/**
+ *
+ * @author Solba
+ */
+public class Teclado {
+    int numeroTeclas;
+    
+    public void teclear(int KeyCode){
+        System.out.println("Se presiono la tecla:"+ KeyCode   );
+        
+    }
+    
+}
---
+++ Sesion_3_agosto/src/sesion_3_agosto/Sesion_3_agosto.java
@@ -0,0 +1,26 @@
+
+package sesion_3_agosto;
+
+import ico.fes.poo.Teclado;
+
+/**
+ *
+ * @author Solba
+ */
+public class Sesion_3_agosto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Teclado objTeclado1= new Teclado();
+        objTeclado1.teclear(10);
+        Teclado t2=new Teclado();
+        Teclado t3=new Teclado();
+        Teclado t4=new Teclado();
+        Teclado t5=new Teclado();
+
+    }
+    
+}
